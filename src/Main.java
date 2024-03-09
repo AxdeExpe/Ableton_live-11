5066009034ebadc37c5f565128bd5ebe69dfbe99
Kev <103242766+AxdeExpe@users.noreply.github.com>
2024-03-09 12:07:49 +0100

    clean code

--- src/Main.java
+++ src/Main.java
@@ -10,111 +10,64 @@ public class Main extends Thread {
     }
 
     public static void main(String[] var0) {
-        int var1 = Runtime.getRuntime().availableProcessors() - 1;
-        Writer[] var2 = new Writer[var1];
+        int threads = Runtime.getRuntime().availableProcessors() - 1;
+        Writer[] writerArr = new Writer[threads];
 
         while(true) {
-            int var3;
-            for(var3 = 0; var3 < var1; ++var3) {
-                var2[var3] = new Writer();
-                var2[var3].start();
+
+            for(int i = 0; i < threads; ++i) {
+                writerArr[i] = new Writer();
+                writerArr[i].start();
             }
 
-            for(var3 = 0; var3 < var1; ++var3) {
+            for(int i = 0; i < threads; ++i) {
                 try {
-                    var2[var3].join();
-                } catch (InterruptedException var6) {
-                    throw new RuntimeException(var6);
+                    writerArr[i].join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
 
-            String var7 = "infinite_merge.txt";
-            String[] var4 = new String[var1];
+            String mergeFile = "infinite_merge.txt";
+            String[] fileList = new String[threads];
 
-            for(int var5 = 0; var5 < var1; ++var5) {
-                var4[var5] = "infinite_" + var2[var5].getId() + ".txt";
+            for(int i = 0; i < threads; ++i) {
+                fileList[i] = "infinite_" + writerArr[i].getId() + ".txt";
             }
 
             System.out.println("Merging...");
-            mergeFiles(var4, var7);
+            mergeFiles(fileList, mergeFile);
         }
     }
 
-    private static void mergeFiles(String[] var0, String var1) {
-        try {
-            FileWriter var2 = new FileWriter(var1, true);
+    private static void mergeFiles(String[] fileNames, String mergedFileName) {
+        try (FileWriter fw = new FileWriter(mergedFileName, true);
+             BufferedWriter bw = new BufferedWriter(fw)) {
 
-            try {
-                BufferedWriter var3 = new BufferedWriter(var2);
-
-                try {
-                    String[] var4 = var0;
-                    int var5 = var0.length;
-
-                    for(int var6 = 0; var6 < var5; ++var6) {
-                        String var7 = var4[var6];
-
-                        try {
-                            BufferedReader var8 = new BufferedReader(new FileReader(var7));
-
-                            String var9;
-                            try {
-                                while((var9 = var8.readLine()) != null) {
-                                    var3.write(var9);
-                                    var3.newLine();
-                                }
-                            } catch (Throwable var14) {
-                                try {
-                                    var8.close();
-                                } catch (Throwable var13) {
-                                    var14.addSuppressed(var13);
-                                }
-
-                                throw var14;
-                            }
-
-                            var8.close();
-                        } catch (IOException var15) {
-                            var15.printStackTrace();
-                        }
+            for (String fileName : fileNames) {
+                try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+                    String line;
+                    // copy each line into the new file
+                    while ((line = br.readLine()) != null) {
+                        bw.write(line);
+                        bw.newLine(); // Neue Zeile einfügen
                     }
-                } catch (Throwable var16) {
-                    try {
-                        var3.close();
-                    } catch (Throwable var12) {
-                        var16.addSuppressed(var12);
-                    }
-
-                    throw var16;
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-
-                var3.close();
-            } catch (Throwable var17) {
-                try {
-                    var2.close();
-                } catch (Throwable var11) {
-                    var17.addSuppressed(var11);
-                }
-
-                throw var17;
             }
-
-            var2.close();
-        } catch (IOException var18) {
-            var18.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        String[] var19 = var0;
-        int var20 = var0.length;
-
-        for(int var21 = 0; var21 < var20; ++var21) {
-            String var22 = var19[var21];
-            File var23 = new File(var22);
-            if (var23.exists()) {
-                if (var23.delete()) {
-                    System.out.println("File deleted: " + var22);
+        // deleting the deprecated files
+        for (String fileName : fileNames) {
+            File file = new File(fileName);
+            if (file.exists()) {
+                if (file.delete()) {
+                    System.out.println("File deleted: " + fileName);
                 } else {
-                    System.out.println("Error while deleting file: " + var22);
+                    System.out.println("Error occurred while deleting file: " + fileName);
                 }
             }
         }
--- src/Writer.java
+++ src/Writer.java
@@ -5,41 +5,39 @@ import java.io.PrintWriter;
 import java.util.Random;
 
 public class Writer extends Thread {
-    private int iterationFilesizeByte = 100000000;
-
-    public Writer() {
-    }
+    private int iterationFilesizeByte = 100000000; // 100MB
 
     public void run() {
-        Random var1 = new Random();
-        byte var2 = 32;
-        byte var3 = 126;
-        String var4 = "infinite_" + Thread.currentThread().getId() + ".txt";
-        PrintWriter var5 = null;
+        Random random = new Random();
+        byte minCharCode = 32;
+        byte maxCharCode = 126;
+        String filename = "infinite_" + Thread.currentThread().getId() + ".txt";
+
+        PrintWriter pw = null;
 
         try {
-            var5 = new PrintWriter(new FileOutputStream(var4, true));
+            pw = new PrintWriter(new FileOutputStream(filename, true));
         } catch (FileNotFoundException var13) {
-            throw new RuntimeException("File " + var4 + " not found!", var13);
+            throw new RuntimeException("File " + filename + " not found!", var13);
         }
 
-        long var6 = 0L;
-        int var8 = 0;
+        long iterator = 0L;
+        int innerIterator = 0;
 
-        long var11;
+        long filesize;
         do {
             do {
-                ++var6;
-                int var9 = var1.nextInt(var3 - var2 + 1) + var2;
+                ++iterator;
+                int var9 = random.nextInt(maxCharCode - minCharCode + 1) + minCharCode;
                 char var10 = (char)var9;
-                var5.write(var10);
-            } while(var6 % (long)this.iterationFilesizeByte != 0L);
+                pw.write(var10);
+            } while(iterator % (long)this.iterationFilesizeByte != 0L);
 
-            var11 = (new File(var4)).length();
-            System.out.println("Filesize " + var4 + ": " + var11);
-        } while(var11 < (long)(this.iterationFilesizeByte * var8));
+            filesize = (new File(filename)).length();
+            System.out.println("Filesize " + filename + ": " + filesize);
+        } while(filesize < (long)(this.iterationFilesizeByte * innerIterator));
 
-        ++var8;
+        ++innerIterator;
         System.out.println("Hopp out!");
     }
 }
